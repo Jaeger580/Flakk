dff8dafb5aa36cfb1fdac90a737cfd940192e9c0
Jakob Jaeger <jaegerj253@gmail.com>
2024-07-03 22:08:02 -0400

    Gun movement changes and Weapons stats
    Implemented new method of moving the gun barrel to match the player's view.
    Added Fire Rate stat.
    Added Ammo Stats.
    Added Reloading.
    Added Overheat Mechanic.

--- Assets/Code/Scripts/Bullet.cs
+++ Assets/Code/Scripts/Bullet.cs
@@ -8,9 +8,22 @@ public class Bullet : MonoBehaviour
     [SerializeField]
     private float speed;
 
+    private int finaleDamage = 0;
+
+    private void Start()
+    {
+        Destroy(gameObject, 5f);
+    }
+
     // Update is called once per frame
     void FixedUpdate()
     {
-        transform.Translate(Vector3.up * Time.deltaTime);
+        transform.Translate(Vector3.up * Time.deltaTime * speed);
+    }
+
+    // Gun will call this method during instantiation. Will get the base damage from the gun.
+    public void setDamage(int damage) 
+    {
+        finaleDamage = damage;
     }
 }
--- Assets/Code/Scripts/Gun Control.cs
+++ Assets/Code/Scripts/Gun Control.cs
@@ -1,45 +1,138 @@
 using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.PlasticSCM.Editor.WebApi;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class GunControl : MonoBehaviour
 {
-    [SerializeField]
-    private float sensitivity = 5f;
-
-    [SerializeField]
-    private float gunRotateSpeed = 5f;
 
     private Camera mainCamera;
+    private Vector2 mouseInput;
+    private float vertRotation;
+    private float horizRotation;
 
+    private float fireRateTimer;
+    private int currentClip = 0;
+    private float reloadTimer;
+    private float currentHeat = 1f;
+    private bool overHeating = false;
+
+
+
+    private bool isShooting = false;
+    private bool isReloading = false;
+
+    [Header("Game Objects")]
     [SerializeField]
     private GameObject gunCamera;
-
+    [SerializeField]
+    private GameObject pivotPoint;
     [SerializeField]
     private GameObject gunBase;
     [SerializeField]
     private GameObject gunBarrel;
     [SerializeField]
     private GameObject gunBulletPoint;
-
-    private Vector2 mouseInput;
-    private float vertRotation;
-    private float horizRotation;
-
     [SerializeField]
     private GameObject bulletPrefab;
+
+    [Header("Weapon Movement")]
+    [SerializeField]
+    private float sensitivity = 5f;
+    [SerializeField]
+    private float gunRotateSpeed = 5f;
+
+    [Header("Gun Stats")]
+    [SerializeField]
+    private int baseDamage = 5;
+    [SerializeField]
+    private float fireRate = 1f;
+    [SerializeField]
+    private float reloadTime = 1f;
+    [SerializeField]
+    private int clipSize = 1;
+    [SerializeField]
+    private float maxheat = 100f;
+    [SerializeField]
+    private float coolDownSpeed = 1f;
+    [SerializeField]
+    private float overHeatSpeed = 1f;
+
+
+
+
+
+
+
+
 
     private void Start()
     {
         mainCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+        fireRateTimer = fireRate;
+        currentClip = clipSize;
     }
 
     private void Update()
     {
-        HandleLook(mouseInput);
+        if (fireRateTimer < fireRate)
+        {
+            fireRateTimer += Time.deltaTime;
+        }
+
+        if (!isShooting)
+        {
+            if (currentHeat > 0 & !overHeating)
+            {
+                currentHeat -= coolDownSpeed * Time.deltaTime;
+            }
+            else if (overHeating && currentHeat > 0) 
+            {
+                currentHeat -= overHeatSpeed * Time.deltaTime;
+            }
+
+            if(currentHeat <= 0 && overHeating) 
+            {
+                overHeating = false;
+            }
+
+            Debug.Log(currentHeat);
+        }
+
+        if (!isReloading) 
+        {
+            HandleLook(mouseInput);
+
+            if (isShooting && fireRateTimer >= fireRate && currentClip > 0)
+            {
+                Fire();
+            }
+        }
+        else if(isReloading)
+        {
+            if(reloadTimer < reloadTime) 
+            {
+                reloadTimer += Time.deltaTime;
+            }
+            else if(currentClip < clipSize)
+            {
+                reloadTimer = 0;
+                currentClip++;
+                Debug.Log(currentClip);
+            }
+            else if(currentClip == clipSize) 
+            {
+                isReloading = false;
+            }
+        }
+
+
     }
+
+    // Starts off faster than slows down to normal pace as approaches center.
 
     private void HandleLook(Vector2 Input)
     {
@@ -52,9 +145,11 @@ public class GunControl : MonoBehaviour
         vertRotation = Mathf.Clamp(vertRotation, -85, 0);
 
         //mainCamera.transform.Rotate(0, horizRotation, 0);
-        gunCamera.transform.localRotation = Quaternion.Euler(vertRotation, horizRotation, 0);
+        pivotPoint.transform.localRotation = Quaternion.Euler(vertRotation, horizRotation, 0);
 
-        gunBase.transform.rotation = Quaternion.RotateTowards(gunBase.transform.rotation, gunCamera.transform.rotation, gunRotateSpeed * Time.deltaTime);
+        var angle = Quaternion.Angle(gunBase.transform.rotation, pivotPoint.transform.rotation);
+
+        gunBase.transform.rotation = Quaternion.RotateTowards(gunBase.transform.rotation, pivotPoint.transform.rotation, gunRotateSpeed * Time.deltaTime * angle);
     }
 
     public void Look(InputAction.CallbackContext context)
@@ -64,11 +159,55 @@ public class GunControl : MonoBehaviour
 
     public void Shoot(InputAction.CallbackContext context)
     {
+        if (!overHeating)
+        {
+            if (context.started)
+            {
+                isShooting = true;
+            }
+
+            else if (context.canceled)
+            {
+                isShooting = false;
+            }
+        }
+    }
+
+    public void Reload(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            reloadTimer = 0f;
+            isReloading = true;
+        }
+
+        else if (context.canceled)
+        {
+            isReloading = false;
+        }
+    }
+
+    private void Fire() 
+    {
         Quaternion gunRotation = gunBase.transform.rotation;
 
-        gunRotation *= Quaternion.Euler(90,0,0);
+        gunRotation *= Quaternion.Euler(90, 0, 0);
 
         Vector3 camCenter = mainCamera.ViewportToWorldPoint(new Vector3(1f, 1f, 0));
         GameObject bulletInstance = Instantiate(bulletPrefab, gunBulletPoint.transform.position, gunRotation);
+        bulletInstance.GetComponent<Bullet>().setDamage(baseDamage);
+
+        currentClip--;
+        currentHeat += 25;
+
+        if(currentHeat >= maxheat) 
+        {
+            overHeating = true;
+            isShooting = false;
+        }
+
+        Debug.Log(currentClip);
+
+        fireRateTimer = 0;
     }
 }
