1f6e74f38026cf9e82cf9f8bce0b681a42edd6f5
atomwolve <35859306+atomwolve@users.noreply.github.com>
2024-07-08 22:13:30 -0400

    Make it make sense

---
+++ Assets/Code/Scripts/PROTO_DamageRedirect.cs
@@ -0,0 +1,13 @@
+﻿using JO.AI;
+using UnityEngine;
+
+public class PROTO_DamageRedirect : MonoBehaviour, IDamagable
+{
+    [SerializeField] private Light_Infantry actualHealthComponent;
+
+    public void TakeDamage(float _damage)
+    {
+        Debug.Log($"{gameObject.name} got hit, redirecting to {actualHealthComponent.gameObject.name}");
+        actualHealthComponent.TakeDamage(_damage);
+    }
+}
