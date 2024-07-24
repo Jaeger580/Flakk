258236ab7b10118a014719ad39372cee5104ed99
Isaac Brunt <47149171+CactusDam@users.noreply.github.com>
2024-07-23 22:51:19 -0400

    All of my changes
    -Heavy and light fighter particle trails
    -Bullet particle trails
    -Muzzle VFX now spawn independently of muzzle movement
    -Muzzle VFX delete themselves after 4f
    -Player Mesh Renderer is disabled to hide the capsule
    -Game cabinets now come without extra cables
    -Cabling is available for the cabinets, made to be above and behind (see the VFX Testing Scene for example)
    -Muzzle smoke is now thinner (more translucent)

--- Assets/Code/Scripts/Gun Control.cs
+++ Assets/Code/Scripts/Gun Control.cs
@@ -71,10 +71,7 @@ public class GunControl : MonoBehaviour
     private FloatReference overheatRate;
 
     [SerializeField]
-    private ParticleSystem[] vfx=new ParticleSystem[4];
-
-    [SerializeField]
-    private Light vfxLight;
+    public GameObject vfxPrefab;
 
     private void OnDisable()
     {
@@ -300,6 +297,8 @@ public class GunControl : MonoBehaviour
         GameObject bulletInstance = Instantiate(bulletPrefab.Value, gunBulletPoint.transform.position, gunRotation);
         bulletInstance.GetComponent<Bullet>().SetDamage(baseDamage.Value);
 
+        GameObject vxfInstance = Instantiate(vfxPrefab, bulletInstance.transform.position, bulletInstance.transform.rotation);
+
         if(AudioManager.instance != null) 
         {
             AudioManager.instance.ForcePlay("Shoot");
@@ -317,11 +316,6 @@ public class GunControl : MonoBehaviour
         }
 
         fireRateTimer = Time.time;
-
-        foreach(ParticleSystem ps in vfx)
-        {
-            ps.Play();
-        }
         
     }
 }
