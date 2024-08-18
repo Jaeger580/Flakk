dac5d7fc76e52c912fa55f3c0248435b67283a67
Jakob Jaeger <jaegerj253@gmail.com>
2024-08-17 23:17:34 -0400

    Clean Up part 1

--- Assets/Code/Scripts/Enemy_Formation_Creator.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-[CreateAssetMenu(fileName = "EF - ", menuName = "Wave System/Enemy Formation")]
-public class Enemy_Formation_Creator : ScriptableObject
-{
-    public GameObject[] formationUnitPrefabs;
-    public Vector3[] spawnRanges;
-    public WebSelection preferredWeb;
-}
---
+++ Assets/Code/Scripts/Unsorted/IDamagable.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IDamagable 
+{
+    public void TakeDamage(int _damage);
+}
