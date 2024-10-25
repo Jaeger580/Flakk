a3da6433b21296b22e6b6c18f37aae60490bd7c8
Jakob Jaeger <jaegerj253@gmail.com>
2024-10-25 00:29:51 -0400

    Large Enemy Start
    Created a new large enemy made up of multiple components.
    Most components need proper implementation.
    Collisions need to be set up correctly to avoid issues.
    Need to speak to allan about handling armor and damage.

---
+++ Assets/Game/Enemies/Prototypes/Components/Pilot.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pilot : DestructablePart
+{
+    // When destroyed, kill enemy.
+    public override void TriggerSpecialDebuff()
+    {
+        // Need a way to apply damage or directly trigger death. Need to speak to allan.
+    }
+}
