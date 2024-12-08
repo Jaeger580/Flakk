f8067978216366812d7a60338966f3a35efd8c33
Jaeger580 <91155249+Jaeger580@users.noreply.github.com>
2024-12-08 03:29:54 -0500

    Audio Part 2
    Implemented temp Audio for shooting, reloading, and hitting enemies.
    Created a custom audio script for player audio.
    Custom audio script methods play audio at slightly randomized pitches.

--- Assets/Game/Gun/Gun Systems/Ammo System/Base Gun Ammo/Base Gun Standard Ammo/BaseGunStandardAmmo.cs
+++ Assets/Game/Gun/Gun Systems/Ammo System/Base Gun Ammo/Base Gun Standard Ammo/BaseGunStandardAmmo.cs
@@ -18,10 +18,13 @@ public class BaseGunStandardAmmo : BaseGunAmmo, IEffect
         if (!TriggerEffect(p)) return;
 
         //do vfx/sfx and other cleanup IF it lands correctly
-        foreach(AudioClip clip in sfxOnImpact) 
-        {
-            AudioSource.PlayClipAtPoint(clip, p.CollisionInfo.transform.position);
-        }
+
+        CustomAudio.PlayClipAt(this.GetComponent<AudioSource>(), p.CollisionInfo.transform.position);
+
+        //foreach(AudioClip clip in sfxOnImpact) 
+        //{
+        //    AudioSource.PlayClipAtPoint(clip, p.CollisionInfo.transform.position);
+        //}
 
         Destroy(this.gameObject);
     }
