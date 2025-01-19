29668c2acc5b855d4ff21b159d049d933ff2875c
Jakob Jaeger <jaegerj253@gmail.com>
2025-01-18 22:47:20 -0500

    Update to Wing script
    Resistances do not seem to be working as expected. Needs updated

--- Assets/Game/Enemies/Prototypes/Components/Wing.cs
+++ Assets/Game/Enemies/Prototypes/Components/Wing.cs
@@ -1,15 +1,28 @@
+using GeneralUtility.CombatSystem;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Wing : DestructablePart
 {
+    [SerializeField]
+    private float speedMultipler = 0.90f;
+
+    [SerializeField]
+    private int bonusDamage = 2;
+
     public override void TriggerSpecialDebuff()
     {
+        CombatPacket packet = new CombatPacket();
+        packet.SetDamage(bonusDamage, this);
+
         localResistance = 100;
-        mainResistance = 50;
+        mainResistance = 100;
 
         SwapParts();
+        mainBody.GetComponent<Enemy>().SpeedMulti(speedMultipler);
+        mainBody.GetComponent<Enemy>().ApplyDamage(packet);
+
         //mainBody.speed--;
         //trigger VFX
         //enable/disable model
