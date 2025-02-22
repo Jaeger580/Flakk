fb4f8d3e12c85d512d1ce386009f92f011043206
atomwolve <35859306+atomwolve@users.noreply.github.com>
2025-02-21 21:17:13 -0500

    Ammo terminal mostly

--- Assets/Game/Gun/Gun Systems/Ammo System/AmmoType.cs
+++ Assets/Game/Gun/Gun Systems/Ammo System/AmmoType.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Ammo System/Ammo Type", fileName = "Ammo Type - ", order = 0)]
 public class AmmoType : ScriptableObject
 {
+    public string ammoName;
+    public string ammoDesc;
     public GameObjectVariable bulletObj;
     public IntVariable damage;
     public GameEvent ammoTypeFiredEvent;
     public AudioClip[] shotSounds;
+
+    public AmmoCrateInteract cratePrefab;
+    public int crateCost;
     //public IntVariable
 }
