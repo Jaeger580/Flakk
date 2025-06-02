446b82d639b367c077962da4c6b08e6478fb758e
Jakob Jaeger <jaegerj253@gmail.com>
2025-06-01 22:53:25 -0400

    Over Heat Health System
    Added new health system with overheat mechanic.
    When enemies attack, they build up heat. When that heat level reaches 700 out of 1000, the player's hull begins to take damage.
    When players hull integrity hits 0, they lose the same way as before.

--- Assets/Game/MotherShip/MothershipHealth.cs
+++ Assets/Game/MotherShip/MothershipHealth.cs
@@ -1,12 +1,20 @@
 ﻿using GeneralUtility.EditorQoL;
 using GeneralUtility.GameEventSystem;
 using GeneralUtility.VariableObject;
+using System.Collections;
 using UnityEngine;
 
 public class MothershipHealth : MonoBehaviour, IDamagable
 {
     [SerializeField] private IntReference maxHealth, currentHealth;
     [SerializeField] private GameEvent healthChangeEvent, deathEvent, winEvent;
+    private bool hasLost = false;
+
+    // Variables for over heat system
+    [SerializeField] private IntReference maxHeat, currentHeat, overHeatThresholdPercent;
+    [SerializeField] private float currentCoolDown, coolDownDelay, coolDownRate;
+    [SerializeField] private int damageRate; // damage per second.
+    [SerializeField] private bool isOverHeating;
 
     private void Start()
     {
@@ -17,26 +25,115 @@ public class MothershipHealth : MonoBehaviour, IDamagable
         winEvent.RegisterListener(winListener);
 
         ResetHealth();
+        StartCoroutine(HullDamage(damageRate));
+        StartCoroutine(CoolDownTracker(coolDownRate));
+    }
+
+    private void FixedUpdate()
+    {
+        // heat values are ints that needed to be calculated as floats
+        float currentHeatPercent = ((float)currentHeat.Value / maxHeat.Value) * 100f;
+
+        // if the current heat is greater than a specified percent of the max heat, then we are over heating.
+        if ((currentHeatPercent >= overHeatThresholdPercent.Value))
+        {
+            if (!isOverHeating)
+            {
+                isOverHeating = true;
+                Debug.Log("SHIP OVERHEATING!!!!");
+            }
+        }
+        else if(isOverHeating)
+        {
+            isOverHeating = false;
+        }
+
+        // currentCoolDown is reset whenever the player is hit. If they go long enough without getting hit, they begin to cool down.
+        if (currentCoolDown < coolDownDelay)
+        {
+            currentCoolDown += Time.deltaTime;
+        }
     }
 
     private void ResetHealth()
     {
+        StopAllCoroutines();
+        hasLost = false;
         currentHealth.Value = maxHealth.Value;
+        currentHeat.Value = 0;
         healthChangeEvent?.Trigger();
     }
 
     public void TakeDamage(int _damage)
     {
-        currentHealth.Value -= _damage;
-        if (currentHealth.Value <= 0) currentHealth.Value = 0;
+        currentHeat.Value += _damage;
+        if (currentHeat.Value > maxHeat.Value) currentHeat.Value = maxHeat.Value;
+
+        currentCoolDown = 0f;
 
         healthChangeEvent?.Trigger();
 
-        if (currentHealth.Value <= 0)
+        Debug.Log("Current Heat raised to " + currentHeat.Value);
+
+        // Old health system before over heat system:
+        //currentHealth.Value -= _damage;
+        //if (currentHealth.Value <= 0) currentHealth.Value = 0;
+
+        //healthChangeEvent?.Trigger();
+
+        //if (currentHealth.Value <= 0)
+        //{
+        //    Debug.Log("You lose!");
+        //    deathEvent?.Trigger();
+        //}
+        //else Debug.Log($"Mothership now at {currentHealth.Value} health.");
+    }
+
+    // Co-routine that deals damage to the main health of the mothership if it is currently over heating.
+    private IEnumerator HullDamage(float healthLossRate) 
+    {
+        while (true) 
         {
-            Debug.Log("You lose!");
-            deathEvent?.Trigger();
+            if (isOverHeating)
+            {
+                currentHealth.Value -= 1;
+
+                if (currentHealth.Value <= 0 && !hasLost)
+                {
+                    Debug.Log("You lose!");
+                    deathEvent?.Trigger();
+                    hasLost = true;
+                }
+                else 
+                {
+                    //Debug.Log($"Mothership now at {currentHealth.Value} health.");
+                    Debug.Log("Hull integrity at " + currentHealth.Value + "%.");
+                }
+
+                yield return new WaitForSeconds(healthLossRate);
+            }
+            else
+            {
+                yield return null;
+            }
         }
-        else Debug.Log($"Mothership now at {currentHealth.Value} health.");
+    }
+
+    private IEnumerator CoolDownTracker(float heatCoolDownRate) 
+    {
+        while (true) 
+        {
+            // If the cool down delay time has passed, cool down at set rate.
+            if (currentCoolDown >= coolDownDelay && currentHeat.Value > 0) 
+            {
+                currentHeat.Value -= 1;
+                Debug.Log("Current Heat lowered to " + currentHeat.Value);
+                yield return new WaitForSeconds(heatCoolDownRate);
+            }
+            else 
+            {
+                yield return null;
+            }
+        }
     }
 }
