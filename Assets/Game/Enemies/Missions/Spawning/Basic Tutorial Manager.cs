3bd65daf6e9b25ca18a22cb91d8ccb557b69fc4e
Jakob Jaeger <jaegerj253@gmail.com>
2025-08-16 23:14:59 -0400

    More Coding

--- Assets/Game/Enemies/Missions/Spawning/Basic Tutorial Manager.cs
+++ Assets/Game/Enemies/Missions/Spawning/Basic Tutorial Manager.cs
@@ -19,7 +19,44 @@ public class BasicTutorialManager : MonoBehaviour
 
     private List<Tutorials> tutorialQueue;
 
-    private void addToQueue(TutorialSeries series) 
+    [SerializeField]
+    private GameEvent nextTutorialEvent;
+    private GameEventListener tutorialListener;
+
+    private void Start()
+    {
+        // Inititial an event listener for cycling through tutorials.
+        tutorialListener = gameObject.AddComponent<GameEventListener>();
+        tutorialListener = gameObject.AddComponent<GameEventListener>();
+        tutorialListener.Events.Add(nextTutorialEvent);
+        tutorialListener.Response = new();
+        tutorialListener.Response.AddListener(() => NextTutorial());
+
+        // register
+        nextTutorialEvent.RegisterListener(tutorialListener);
+
+        // For each of the series of tutorials, create an event listener that will add the series to the queue.
+        foreach (TutorialSeries series in tutorialList) 
+        {
+            // set up a new event listener for the new event
+            GameEventListener listener = new GameEventListener();
+
+            listener = gameObject.AddComponent<GameEventListener>();
+            listener.Events.Add(series.targetEvent);
+            listener.Response = new();
+            listener.Response.AddListener(() => AddToQueue(series));
+
+            // register
+            series.targetEvent.RegisterListener(listener);
+        }
+    }
+
+    private void NextTutorial() 
+    {
+
+    }
+
+    private void AddToQueue(TutorialSeries series) 
     {
         List<Tutorials> tutorials = series.tutorials;
 
@@ -28,7 +65,6 @@ public class BasicTutorialManager : MonoBehaviour
             tutorialQueue.Add(t);
         }
     }
-
 }
 
 [Serializable]
@@ -45,5 +81,12 @@ public class TutorialSeries
     public List<Tutorials> tutorials;
 
     public GameEvent targetEvent;
+
+    private bool triggered = false;
+
+    public void WasTriggered() 
+    {
+        triggered = true;
+    }
 }
 
