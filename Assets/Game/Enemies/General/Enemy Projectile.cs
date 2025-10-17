8c60705668cbe977b14a82b3ddd27babc828f6ee
Jakob Jaeger <jaegerj253@gmail.com>
2025-10-16 23:20:54 -0400

    Enemy Projectile VFX setup

--- Assets/Game/Enemies/General/Enemy Projectile.cs
+++ Assets/Game/Enemies/General/Enemy Projectile.cs
@@ -10,6 +10,10 @@ public abstract class EnemyProjectile : MonoBehaviour
     [SerializeField]
     protected float travelSpeed;
 
+    [SerializeField]
+    protected MeshRenderer bulletGraphic;
+    [SerializeField]
+    protected GameObject sonicVFX;
 
     protected int targetLayer;
     protected virtual void Start() 
@@ -24,12 +28,28 @@ public abstract class EnemyProjectile : MonoBehaviour
         {
             //Debug.Log(damage + " damage dealt to weakpoint");
             other.gameObject.GetComponentInParent<MothershipHealth>().ApplyDamage(damage);
-            Destroy(this.gameObject);
+            //Destroy(this.gameObject);
+            DestroySelf();
         }
     }
 
     protected virtual void FixedUpdate() 
     {
         transform.position += transform.forward * travelSpeed * Time.deltaTime;
+    }
+
+    protected private IEnumerator DestroySelf()
+    {
+        this.gameObject.GetComponent<Collider>().enabled = false;
+        bulletGraphic.enabled = false;
+        Rigidbody rb = this.gameObject.GetComponent<Rigidbody>();
+        rb.velocity = Vector3.zero;
+        rb.isKinematic = true;
+
+
+        sonicVFX.GetComponent<ParticleSystem>().Stop();
+
+        yield return new WaitForSeconds(.5f);
+        Destroy(this.gameObject);
     }
 }
